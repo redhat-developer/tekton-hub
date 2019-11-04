b338eed901a72541fe8137e278214fb44d6bcc0e
chetan-rns <chetanrns1997@gmail.com>
2019-11-04 15:44:40 +0530

    Developed packages for extracting README and YAML files

---
+++ pkg/polling/authenticate.go
@@ -0,0 +1,19 @@
+package polling
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+// Authenticate and return a Github client
+func Authenticate() (*github.Client, context.Context) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "bb8ef3ba60abf17d10002562b3bf889e3a9a2c2a"},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	return client, ctx
+}
