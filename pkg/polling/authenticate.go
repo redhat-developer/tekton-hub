21aaca2fa712d1c238e5ab6dc35f4ae0b1223a29
chetan-rns <chetanrns1997@gmail.com>
2019-11-05 16:10:52 +0530

    Added new token credentials

--- pkg/polling/authenticate.go
+++ pkg/polling/authenticate.go
@@ -11,7 +11,8 @@ import (
 func Authenticate() (*github.Client, context.Context) {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "bb8ef3ba60abf17d10002562b3bf889e3a9a2c2a"},
+		// &oauth2.Token{AccessToken: "bb8ef3ba60abf17d10002562b3bf889e3a9a2c2a"},
+		&oauth2.Token{AccessToken: "1a44fd58ca485074e8c16ba303def5f718feef45"},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
