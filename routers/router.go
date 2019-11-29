8b965cbb701bb0c2f873ab343914c6bec07fa919
chetan-rns <chetanrns1997@gmail.com>
2019-11-29 15:14:48 +0530

    Added CORS handler

--- routers/router.go
+++ routers/router.go
@@ -15,5 +15,5 @@ func HandleRouters(router *mux.Router) {
 	router.Path("/tasks").Queries("tags", "{tags}").HandlerFunc(api.GetAllFilteredTasksByTag).Methods("GET")
 	router.Path("/tasks").Queries("category", "{category}").HandlerFunc(api.GetAllFilteredTasksByCategory).Methods("GET")
 	router.HandleFunc("/tasks", api.GetAllTasks).Methods("GET")
-	router.HandleFunc("/download/{id}", api.DownloadFile).Methods("GET")
+	router.HandleFunc("/download/{id}", api.DownloadFile).Methods("POST")
 }
