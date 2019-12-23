7e903d658d30cd93c84bf2de1cb49a4392cec6d1
chetan-rns <chetanrns1997@gmail.com>
2019-12-23 16:28:50 +0530

    Updated validation API for pipelines

--- main.go
+++ main.go
@@ -23,19 +23,35 @@ type ValidationResponse struct {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/validate/task/{fileName}", validateTask).Methods("POST")
+	router.HandleFunc("/validate/{type}/{fileName}", validate).Methods("POST")
 	http.ListenAndServe(":5001", router)
 }
-
-func checkSchema(fileName string) error {
-	b, err := ioutil.ReadFile(fileName) // just pass the file name
+func checkPipelineSchema(fileName string) error {
+	b, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Println(err)
+	}
+	var pipeline v1alpha1.Pipeline
+	err = yaml.Unmarshal(b, &pipeline)
+	if err != nil {
+		log.Println("Invalid Pipeline schema")
+		return err
+	}
+	ctx := context.Background()
+	if err := pipeline.Validate(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+func checkTaskSchema(fileName string) error {
+	b, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Println(err)
 	}
 	var task v1alpha1.Task
 	err = yaml.Unmarshal(b, &task)
 	if err != nil {
-		log.Println("Error")
+		log.Println("Invalid Task schema")
 		return err
 	}
 	ctx := context.Background()
@@ -46,8 +62,7 @@ func checkSchema(fileName string) error {
 	return nil
 }
 
-// CheckLintErrors checks lint errors based on .yamllint config file
-func validateTask(w http.ResponseWriter, r *http.Request) {
+func validate(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	fileName := mux.Vars(r)["fileName"]
 	file, header, err := r.FormFile(fileName)
@@ -64,12 +79,19 @@ func validateTask(w http.ResponseWriter, r *http.Request) {
 	if result != "Success\n" {
 		response := ValidationResponse{false, result}
 		json.NewEncoder(w).Encode(response)
+		os.Remove(fileName)
+		return
 	}
-	err = checkSchema(fileName)
-	os.Remove(fileName)
+	resourceType := mux.Vars(r)["type"]
+	if resourceType == "task" {
+		err = checkTaskSchema(fileName)
+	} else if resourceType == "pipeline" {
+		err = checkPipelineSchema(fileName)
+	}
 	if err != nil {
 		response := ValidationResponse{false, err.Error()}
 		json.NewEncoder(w).Encode(response)
+		return
 	}
 	resp := ValidationResponse{true, "Success"}
 	json.NewEncoder(w).Encode(resp)
