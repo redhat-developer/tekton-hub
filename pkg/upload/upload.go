65d17ec60b16880c8a2e7438040c5dc2e2c4fa83
chetan-rns <chetanrns1997@gmail.com>
2020-01-22 22:27:26 +0530

    Added database backup files

--- pkg/upload/upload.go
+++ pkg/upload/upload.go
@@ -163,10 +163,10 @@ func doesResourceExist(paths []string, owner string, repositoryName string, reso
 // NewUploadPipeline handles uploading of new task/pipeline
 func NewUploadPipeline(name string, description string, objectType string, tags []string, github string, userID int) interface{} {
 	log.Println(objectType)
-	// isSameResource := models.CheckSameResourceUpload(userID, name)
-	// if isSameResource {
-	// 	return map[string]interface{}{"status": false, "message": objectType + " already exists"}
-	// }
+	isSameResource := models.CheckSameResourceUpload(userID, name)
+	if isSameResource {
+		return map[string]interface{}{"status": false, "message": objectType + " already exists"}
+	}
 	// Get owner and repository name from github link
 	owner, repositoryName := GetGithubOwner(github)
 	// Check if owner and repository name is valid
