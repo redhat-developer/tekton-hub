80d986fc1af74ec19bec2496b1ae7c02922cfa0c
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-02-24 22:48:56 +1000

    UI/Image: allow config.js to be world written
    Openshift runs containers as random users so the config.js that is
    generated at the time container is run should be writable by everyone
    i.e.: 666 file mask

--- frontend/image/start.sh
+++ frontend/image/start.sh
@@ -3,8 +3,11 @@ set -e -u -o pipefail
 
 CONFIG_JS=/usr/share/nginx/html/config.js
 echo "Current ENV"
+echo '----------------------------------------------'
 cat $CONFIG_JS
+echo '..............................................'
 ls -l  $CONFIG_JS
+echo "whoami: $(id)"
 echo '----------------------------------------------'
 
 cat <<-EOF > $CONFIG_JS
