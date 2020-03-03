8e7fd993a0e9e7bf2d8d289be0c54d63b82f8450
Shivam Mukhade <shivammukhade@gmail.com>
2020-03-03 13:15:11 +0530

    UI: Display Categories instead of Tags

--- frontend/src/components/filter/Filter.tsx
+++ frontend/src/components/filter/Filter.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable consistent-return */
 import React,
-{useState,
-  useEffect}
+{
+  useState,
+  useEffect,
+}
   from 'react';
-import './filter.css';
-import {Checkbox,
+import {
+  Checkbox,
   Tooltip,
   Button,
 } from '@patternfly/react-core/dist/js/components';
@@ -12,76 +14,77 @@ import {API_URL} from '../../constants';
 import {InfoCircleIcon} from '@patternfly/react-icons';
 import store from '../redux/store';
 import {FETCH_TASK_SUCCESS} from '../redux/Actions/TaskActionType';
+import './filter.css';
 
-const Filter: React.FC = (props:any) => {
-  const [, setTags] = useState();
-  const [status, setStatus]=useState();
-  const [max, setMax]=useState(18);
-  const [, setX]=useState(1); //
-  const [clear, setClear]=useState(' ');
-  const [show, setShow]=useState('Show all');
-  const tagitem :any =[{id: '1000', value: 'task', isChecked: false},
+const Filter: React.FC = (props: any) => {
+  const [categoryData, setCategoryData] = useState();
+  const [status, setStatus] = useState();
+  const [clear, setClear] = useState(' ');
+  const filterItem: any = [{id: '1000', value: 'task', isChecked: false},
     {id: '1001', value: 'pipeline', isChecked: false},
     {id: '1002', value: 'verified', isChecked: false}];
-  const taglist=(data:any) =>{
-    data.map((it:any ) =>
-      tagitem.push({id: String(it.id), value: it.name, isChecked: false}));
-    return tagitem;
+
+  //  function for adding categories to filteritem
+  const addCategory = (categoryData: any) => {
+    Object.keys(categoryData).map((categoryName: string, index: number) =>
+      filterItem.push(
+          {id: index.toString(), value: categoryName, isChecked: false},
+      ));
+    return categoryData;
   };
   useEffect(() => {
-    // const fetchData = async () => {
-    fetch(`${API_URL}/tags`)
+    fetch(`${API_URL}/categories`)
         .then((res) => res.json())
-        .then((data) => setTags(taglist(data)));
-    setStatus(tagitem);
+        .then((categoryData) => setCategoryData(addCategory(categoryData)));
+    setStatus(filterItem);
     // eslint-disable-next-line
   }, []);
-  // / dummy function for refreshing component
-  const ddff=() =>{
-    setX((x) => x+1);
-  };
-  //  formation of filter url for fetching task and pipelines
-  const filterurl=(e:any) =>{
-    let typeurl='all';
-    let verifiedurl='all';
-    let tagsurl = '';
 
-    status.forEach((it:any) => {
-      if (it.id === e.target.id) {
-        it.isChecked=e.target.checked;
+  //  formation of filter url  for calling filterAPi to
+  //  fetching task and pipelines
+  const filterApi = (event: any) => {
+    let typeurl = 'all';
+    let verifiedurl = 'all';
+    let categoryurl = '';
+
+
+    status.forEach((it: any) => {
+      if (it.id === event.target.id) {
+        it.isChecked = event.target.checked;
       }
     },
     );
-    setStatus(status);
-    ddff();
+    setStatus(status); // updating the status
+
     const temptype = status.slice(0, 2);
-    const tempverified=status.slice(2, 3);
-    const temptags=status.slice(3);
+    const tempverified = status.slice(2, 3);
+    const tempcategory = status.slice(3);
     if (tempverified[0].isChecked === true) {
-      verifiedurl='true';
+      verifiedurl = 'true';
     }
     if (temptype[0].isChecked === true) {
-      typeurl='task';
+      typeurl = 'task';
     }
     if (temptype[1].isChecked === true) {
-      typeurl ='pipeline';
+      typeurl = 'pipeline';
     }
     if ((temptype[0].isChecked === true) &&
-       (temptype[1].isChecked === true)) {
+      (temptype[1].isChecked === true)) {
       typeurl = 'all';
     }
-    temptags.forEach((it:any) =>{
-      if (it.isChecked === true) {
-        tagsurl+=it.value+'|';
+    tempcategory.map((item: any) => {
+      if (item.isChecked === true) {
+        categoryData[item.value].map((categoryitem: any) => {
+          categoryurl = categoryurl + categoryitem + '|';
+        });
       }
-      // if (it.isChecked === true) {
-      //   setClear('ClearAll');
-      // }
     });
-    let flag:any= false;
-    status.forEach((it:any) =>{
+
+    // for displaying clear filter options
+    let flag: any = false;
+    status.forEach((it: any) => {
       if (it.isChecked === true) {
-        flag=true;
+        flag = true;
       }
     });
     if (flag === true) {
@@ -91,9 +94,9 @@ const Filter: React.FC = (props:any) => {
     }
 
 
-    fetch(`${API_URL}/resources/${typeurl}/${verifiedurl}?tags=${tagsurl} `)
+    fetch(`${API_URL}/resources/${typeurl}/${verifiedurl}?tags=${categoryurl} `)
         .then((resp) => resp.json())
-        .then((data) =>{
+        .then((data) => {
           store.dispatch(
               {
                 type: FETCH_TASK_SUCCESS,
@@ -103,28 +106,28 @@ const Filter: React.FC = (props:any) => {
         });
   };
 
-  // /   for clearing all checkbox
-  const cleartag=(e:any) =>{
-    status.forEach((it:any) =>{
+
+  //   for clearing all checkbox
+  const clearFilter = (e: any) => {
+    status.forEach((it: any) => {
       if (it.isChecked === true) {
-        it.isChecked=false;
+        it.isChecked = false;
       }
     });
     setStatus(status);
-    ddff();
-    filterurl(e);
+    filterApi(e);
     setClear('');
   };
-  let showresource:any;
+  let resourceType: any;
   if (status !== undefined) {
-    const resource =status.slice(0, 2);
-    showresource = resource.map((it: any, idx :number) => (
-      <div key = {`res-${idx}`} style={{marginBottom: '0.5em'}}>
+    const resource = status.slice(0, 2);
+    resourceType = resource.map((it: any, idx: number) => (
+      <div key={`res-${idx}`} style={{marginBottom: '0.5em'}}>
         <Checkbox
-          onClick={filterurl}
+          onClick={filterApi}
           isChecked={it.isChecked}
           style={{width: '1.2em', height: '1.2em'}}
-          label={it.value[0].toUpperCase()+it.value.slice(1)}
+          label={it.value[0].toUpperCase() + it.value.slice(1)}
           value={it.value}
           name="type"
           id={it.id}
@@ -134,17 +137,17 @@ const Filter: React.FC = (props:any) => {
       </div>
     ));
   }
-  let showverifiedtask:any;
+  let showverifiedtask: any;
   // jsx element for show verifiedtask
-  if ( status !== undefined) {
-    const verifiedtask=status.slice(2, 3);
+  if (status !== undefined) {
+    const verifiedtask = status.slice(2, 3);
     showverifiedtask = verifiedtask.map((it: any, idx: number) => (
-      <div key = {`task-${idx}`} style={{marginBottom: '0.5em'}}>
+      <div key={`task-${idx}`} style={{marginBottom: '0.5em'}}>
         <Checkbox
-          onClick={filterurl}
+          onClick={filterApi}
           isChecked={it.isChecked}
           style={{width: '1.2em', height: '1.2em'}}
-          label={it.value[0].toUpperCase()+it.value.slice(1)}
+          label={it.value[0].toUpperCase() + it.value.slice(1)}
           value={it.value}
           name="verified"
           id={it.id}
@@ -154,69 +157,57 @@ const Filter: React.FC = (props:any) => {
       </div>
     ));
   }
-  // jsx element for show tags
-  let showTags:any ='';
+  // jsx element for showing all categories
+  let categoryList: any = '';
   if (status !== undefined) {
-    const tempstatus=status.slice(3, max);
-    tempstatus.sort((a:any, b:any) =>
-  (a.value> b.value) ? 1 :
-  ((b.value > a.value) ? -1 : 0));
-    showTags=
-     tempstatus.map((it: any) => (
-       <div key = {it} style={{marginBottom: '0.5em'}}>
-         <Checkbox
-           onClick={filterurl}
-           isChecked={it.isChecked}
-           style={{width: '1.2em', height: '1.2em'}}
-           label={it.value[0].toUpperCase()+it.value.slice(1)}
-           value={it.value}
-           name="Tags"
-           id={it.id}
-           aria-label="uncontrolled checkbox example"
+    const tempstatus = status.slice(3);
+    tempstatus.sort((a: any, b: any) =>
+      (a.value > b.value) ? 1 :
+        ((b.value > a.value) ? -1 : 0));
+    categoryList =
+      tempstatus.map((it: any) => (
+        <div key={it} style={{marginBottom: '0.5em'}}>
+          <Checkbox
+            onClick={filterApi}
+            isChecked={it.isChecked}
+            style={{width: '1.2em', height: '1.2em'}}
+            label={it.value[0].toUpperCase() + it.value.slice(1)}
+            value={it.value}
+            name="Tags"
+            id={it.id}
+            aria-label="uncontrolled checkbox example"
 
-         />
-       </div>
-     ));
+          />
+        </div>
+      ));
   }
-  // /  for showing more
-  const seeMore=(e:any) =>{
-    const x=max;
 
-    if (x< status.length) {
-      setMax(status.length);
-      setShow('Show less');
-    } else {
-      setMax((x) => x-13);
-      setShow('Show all');
-    }
-  };
 
   return (
     <div className="filter-size" >
       <h2 style={{marginBottom: '1em'}}>
         {' '}
         <Button component='a' variant='link'
-          onClick={cleartag}> {clear} </Button>{'  '}
+          onClick={clearFilter}> {clear} </Button>
+        {'  '}
+
       </h2>
       <h2 style={{marginBottom: '1em'}}>
         {' '}
         <b>Types</b>{'  '}
       </h2>
-      {showresource}
+      {resourceType}
       <h2 style={{marginBottom: '1em', marginTop: '1em'}}>
         {' '}
         <b>Verified </b>{'  '}
         <Tooltip content={<div>
-           Verified Task and Pipelines by Tekton Catalog</div>}>
+          Verified Task and Pipelines by Tekton Catalog</div>}>
           <InfoCircleIcon />
         </Tooltip>
       </h2>
       {showverifiedtask}
-      <h2 style={{marginBottom: '1em', marginTop: '1em'}}><b> Tags </b></h2>
-      {showTags}
-      <Button component='a' variant='link'
-        onClick={seeMore}>{show} </Button>
-
+      <h2 style={{marginBottom: '1em', marginTop: '1em'}}><b>Categories</b></h2>
+      {categoryList}
     </div>
   );
 };
